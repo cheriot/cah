d292e013e831722e3f545daba36b519854a9e38c
Christopher Heriot <cheriot@gmail.com>
2016-08-16 16:29:46 +0300

    Hello, World deploys to GAE

---
+++ gae-web-worker/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+    res.status(200).send('Hello, horrible world!');
+});
+
+// Start the server
+var server = app.listen(process.env.PORT || '8080', function () {
+  console.log('App listening on port %s', server.address().port);
+  console.log('Press Ctrl+C to quit.');
+});
